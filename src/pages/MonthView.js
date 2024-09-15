7ec978ef64ca68cf98a35c73ebf1366e017fb15b
hyunju1211 <hyunju0411@konkuk.ac.kr>
2024-09-16 00:04:24 +0900

    Fix MonthView bug: allow correct navigation to and date selection in other months

--- src/pages/MonthView.js
+++ src/pages/MonthView.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import Calendar from 'react-calendar';
 import './MonthView.css'; 
 import moment from "moment";
@@ -23,10 +23,11 @@ const MonthView = () => {
     setIsFocused(false);
   };
 
+  //타일 클릭시, 해당 날 반환
   const handleDateChange = (date) => {
     const dateString = moment(date).format("YYYY-MM-DD");
     const monthKey = moment(calendarDate).format("YYYY-MM");
-
+    console.log(date);
     // 현재 달의 선택된 날짜를 저장
     setSavedDates(prevSavedDates => {
       const currentMonthDates = prevSavedDates[monthKey] || [];
@@ -69,31 +70,82 @@ const MonthView = () => {
     setSelectedDates(savedDates[currentMonthKey] || []); // 새로운 달로 이동할 때 해당 달의 선택된 날짜를 불러옴
   }, [calendarDate, savedDates]);
 
+  //드롭다운에서 월(1-12) 선택
   const handleMonthChange = (e) => {
-    const currentMonthKey = moment(calendarDate).format("YYYY-MM");
-
+    
     // 현재 달의 선택된 날짜들을 저장
+    const currentMonthKey = moment(calendarDate).format("YYYY-MM");
+    console.log(currentMonthKey);
+    // 현재 무슨달인지 console.log에 출력
     if (selectedDates.length > 0) {
       setSavedDates(prevSavedDates => ({
         ...prevSavedDates,
         [currentMonthKey]: selectedDates,
       }));
+      console.log("현재 선택된 날짜: "+selectedDates);
+      //새로운 달로 이동하기전에 현재달에서 무슨무슨 날 선택된건지 출력
     }
 
     // 새로운 달로 이동
-    const newMonth = parseInt(e.target.value, 10) - 1;
+    const newMonth = parseInt(e.target.value, 10)-1;
     setSelectedMonth(e.target.value);
+    console.log(newMonth);
+    //무슨 달로 이동한건지 출력
     const newDate = new Date(calendarDate.getFullYear(), newMonth, 1);
     setCalendarDate(newDate);
-
+    console.log(newDate);
+    console.log(calendarDate);
     // 새로운 달로 이동할 때 이전 선택 상태를 불러옴
     const newMonthKey = moment(newDate).format("YYYY-MM");
-    setSelectedDates(savedDates[newMonthKey] || []); 
+    setSelectedDates(savedDates[newMonthKey] || []);
+
   };
 
+  //타일 비활성화 조건->true일때
   const tileDisabled = ({ date, view }) => {
-    return view === 'month' && date.getMonth() !== calendarDate.getMonth();
+    // console.log(date+"랄ㄹ라"); //Sat Oct 05 2024 00:00:00 GMT+0900 (한국 표준시) 이런식
+    // console.log(view+"룰루"); //month
+    // console.log(date.getMonth()+"dddddd"); //9
+    // console.log("ㅇㅇㅇㅇ"+calendarDate.getMonth()); //8???
+    // return view === 'month' && date.getMonth() !== calendarDate.getMonth();
+    // return true;
+    return false; //임시 반환값
+    //return 현재달(맨처음 클릭한달?) =< 지금 클릭한 달 숫자일때만 false반환
   };
+
+  // const DatePicker = ({ activeStartDate }) => {
+  //   const [startDate, setStartDate] = useState(activeStartDate);
+  //   const refCalendarContainer = useRef(null);
+  
+  //   const handleNavigationClick = () => {
+  //     const refNode = refCalendarContainer.current;
+  //     const currentCalendarView = refNode.querySelector('.react-calendar__view');
+  //     const activeMonth = new Date(currentCalendarView?.dataset?.activeStartDate || new Date());
+  //     setStartDate(activeMonth);
+  //   };
+  
+  //   useEffect(() => {
+  //     if (activeStartDate instanceof Date) {
+  //       setStartDate(activeStartDate);
+  //     }
+  //   }, [activeStartDate]);
+  
+  //   useEffect(() => {
+  //     const refNode = refCalendarContainer.current;
+  //     const navigationButtons = refNode.querySelectorAll('.react-calendar__navigation__arrow');
+  
+  //     navigationButtons.forEach(button => {
+  //       button.addEventListener('click', handleNavigationClick);
+  //     });
+  
+  //     return () => {
+  //       navigationButtons.forEach(button => {
+  //         button.removeEventListener('click', handleNavigationClick);
+  //       });
+  //     };
+  //   }, []);
+  
+   
 
   return (
     <div className="main-container">
@@ -128,9 +180,10 @@ const MonthView = () => {
           </select>
         </div>
       </div>
+      {/* <div className="calendar" ref={refCalendarContainer}> */}
       <div className="calendar">
-        <Calendar 
-          value={calendarDate}
+        <Calendar
+          activeStartDate={calendarDate} 
           locale="ko-KR"
           calendarType="gregory"
           onClickDay={handleDateChange}
@@ -138,6 +191,7 @@ const MonthView = () => {
           formatDay={(locale, date) => moment(date).format("DD")}
           showNeighboringMonth={true}
           tileDisabled={tileDisabled}
+          // activeStartDate={startDate}
         /> 
       </div>
     </div>
