5b531b1361386c7f05d78577736268ebf9447f2c
hyunju1211 <hyunju0411@konkuk.ac.kr>
2025-02-07 22:36:38 +0900

    [FIX] Production 환경에서 CSS 레이아웃 어긋남 이슈

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
 content: ["./src/**/*.{js,jsx,ts,tsx}"],
+important: "#__next",
   theme: {
     extend: {
       fontFamily: {
