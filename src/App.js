f7e9cc010c34ef30ef9639e420cdd0589fe133c3
hyunju1211 <hyunju0411@konkuk.ac.kr>
2025-07-28 16:09:53 +0900

    [UPDATE] @stagewise-plugins/react 업그레이드에 따라, 임포트 방식 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { StagewiseToolbar } from "@stagewise/toolbar-react";
-import { ReactPlugin } from "@stagewise-plugins/react";
+import ReactPlugin from "@stagewise-plugins/react";
 import "./App.css";
 import Invite from "./features/invite/pages/Invite";
 import EventCalendar from "./features/calendar/pages/EventCalendar";
